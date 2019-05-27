5e34bab5b15d13d3d1f3e54f80c53c194f3bcb13
mooncrest <pengray4@gmail.com>
2019-05-27 02:41:34 -0400

    Added rotation visualization

--- visualizer.py
+++ visualizer.py
@@ -6,208 +6,340 @@ from matplotlib.widgets import Slider, Button, RadioButtons
 import matplotlib.patches as patches
 from rubiks import *
 from solution import Solution
+STEPS = 4
 
-# default face for red
-col_con = {'R': 'red', 'G': 'green', 'B': 'blue', 'O': 'orange', \
-           'Y': 'yellow', 'W': 'white'}
+def X_rotation_matrix(steps):
+    steps = steps * 2
+    rotation_matrix = np.array([1, 0, 0, 0, np.cos(np.pi/steps), np.sin(np.pi/steps), \
+                                0, -np.sin(np.pi/steps), np.cos(np.pi/steps)])
+    return rotation_matrix.reshape((3, 3))
 
-# x axis is the red face pointing at you
-X = [*[1]*16]
-Y = [*[1, 1/3, -1/3, -1]*4]
-Z = [*[1] * 4, *[1/3]*4, *[-1/3]*4, *[-1]*4]
+def Y_rotation_matrix(steps):
+    steps = steps * 2
+    rotation_matrix = np.array([np.cos(np.pi/steps), 0, np.sin(np.pi/steps), 0, 1, 0, -np.sin(np.pi/steps), 0, np.cos(np.pi/steps)])
+    return rotation_matrix.reshape((3, 3))
 
-def rotate(x, y, z, axis='Z', times=1):
-    '''return a copy of rotated variables default rotation is 90 degrees
-    if times is a multiplier of the rotation angle can be any real number'''
-    New_X = []
-    New_Y = []
-    New_Z = []
-    if axis == 'Z':
-        for a, b, c in zip(x, y, z):
-            New_X.append(np.cos(np.pi * times / 2) * a - np.sin(np.pi * times / 2) * b)
-            New_Y.append(np.sin(np.pi * times / 2) * a + np.cos(np.pi * times / 2) * b)
-            New_Z.append(c)
+def Z_rotation_matrix(steps):
+    steps = steps * 2
+    rotation_matrix = np.array([np.cos(np.pi/steps), -np.sin(np.pi/steps), 0, np.sin(np.pi/steps), np.cos(np.pi/steps), 0, 0, 0, 1])
+    return rotation_matrix.reshape((3, 3))
 
-    elif axis == 'Y':
-        for a, b, c in zip(x, y, z):
-            New_X.append(np.cos(np.pi * times / 2) * a + np.sin(np.pi * times / 2) * c)
-            New_Y.append(b)
-            New_Z.append(- np.sin(np.pi * times / 2) * a + np.cos(np.pi * times / 2) * c)
+translations = [np.array([a, b, c]) for a in (2, 0, -2) for b in (2, 0, -2) for c in (2, 0, -2)]
+points = [np.array([a, b, c]) for a in (1, -1) for b in (1, -1) for c in (1, -1)]
+verticies = [points[0:4], points[4:8], points[0:2] + points[4:6], points[2: 4] + points[6: 8], points[0:8:2], points[1:8:2]] 
+verticies = [i[0: 2] + i[3: 1: -1] for i in verticies]
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+poly3d_objects = []
 
-    return New_X, New_Y, New_Z
+for translation in translations:
+    translated = [[vert + translation for vert in face] for face in verticies]
+    cube = []
+    for col, vert in zip(['red', 'orange', 'blue', 'green', 'white', 'yellow'], translated):   
+        # print(np.array([np.array(i) for i in vert] ))
+        # print([vert])
+        poly3d = Poly3DCollection([np.array([np.array(i) for i in vert])], facecolor=col, linewidths=1)
+        poly3d.set_edgecolor('black')
+        ax.add_collection3d(poly3d)
+        cube.append((poly3d, vert))
+    poly3d_objects.append(cube)
 
-def generate(colour_grid, center, x, y, z):
-    '''generates the faces'''
-    faces = {}
-    a = [np.array([c, d, e]) for c, d, e in zip(x, y, z)]
+def rotation():
+    if ROTATION_ORDER == []:
+        return False
+    rotation = ROTATION_ORDER.pop()
+    if rotation[0] == 'X':
+        rot = X_rotation_matrix(rotation[3])
+    elif rotation[0] == 'Y':
+        rot = Y_rotation_matrix(rotation[3])
+    else:
+        rot = Z_rotation_matrix(rotation[3])
 
-    verts = []
-    for i in [0, 1, 2, 4, 5, 6, 8, 9, 10]:
-        verts.append([a[i], a[i + 1], a[i + 5], a[i + 4]])
+    for cube in poly3d_objects:
+        if any(rotation[2] == round(vert[rotation[1]]) for poly in cube for vert in poly[1]):
+            for poly in cube:
+                for ind, vert in enumerate(poly[1]):
+                    poly[1][ind] = np.matmul(vert, rot) 
+                poly[0].set_verts([poly[1]])
 
-    for ind, c, v in zip(range(9), colour_grid, verts):
-        if f'{center}{ind}' not in faces:
-            faces[f'{center}{ind}'] = Poly3DCollection([v], 
-         facecolors=col_con[c], linewidths=1, edgecolors='black', alpha=1)
-        ax.add_collection3d(faces[f'{center}{ind}'])
+    # rot = Y_rotation_matrix(-10)
+    # for cube in poly3d_objects:
+    #     if any(3 == vert[1] for poly in cube for vert in poly[1]):
+    #         for poly in cube:
+    #             for ind, vert in enumerate(poly[1]):
+    #                 poly[1][ind] = np.matmul(vert, rot) 
+    #             poly[0].set_verts([poly[1]])
+    fig.canvas.draw_idle()    
+    return True
 
-    return faces
+def parse_rotation(rotations, STEPS=STEPS):
+    parsed = []
+    for rot in rotations:
+        if rot[0] + rot[1] in 'RYOWR':
+            if rot[2] == 0:
+                # vert[1] == -3 inverted rotation Y
+                parsed.extend([['Y', 1, 3, -STEPS]] * STEPS)
+            elif rot[2] == 2: 
+                # vert[1] == 3 inverted rotation Y
+                parsed.extend([['Y', 1, -3, -STEPS]] * STEPS)
 
-def plot(rubiks):
-    a = [1, 1, 1, 1, -1, -1, -1, -1]
-    b = [1, 1, -1, -1, 1, 1, -1, -1]
-    c = [1, -1, 1, -1, 1, -1, 1, -1]
-    poly3Dobjects = {}
-    colours = rubiks.visualizer_string()
-    ax.scatter(a, b, c, c='black', marker= ' ')
-    poly3Dobjects.update(generate(colours['G'], 'G', *rotate(X, Y, Z, times=1)))
-    poly3Dobjects.update(generate(colours['O'], 'O', *rotate(X, Y, Z, times=2)))
-    poly3Dobjects.update(generate(colours['B'], 'B', *rotate(X, Y, Z, times=3)))
-    poly3Dobjects.update(generate(colours['Y'], 'Y', *rotate(X, Y, Z, axis = 'Y', times=-1)))
-    poly3Dobjects.update(generate(colours['W'], 'W', *rotate(X, Y, Z, axis = 'Y', times=1)))
-    poly3Dobjects.update(generate(colours['R'], 'R', X, Y, Z))
-    # fig.canvas.draw_idle()
-    return poly3Dobjects
+        elif rot[0] + rot[1] in 'BYGWB':
+            if rot[2] == 0:
+                # vert[0] == 3 inverted rotation X
+                parsed.extend([['X', 0, -3, -STEPS]] * STEPS)
+            elif rot[2] == 2: 
+                # vert[0] == -3 inverted rotation X
+                parsed.extend([['X', 0, 3, -STEPS]] * STEPS)
 
-def update_3d_plot():
-    colours = b.visualizer_string()
-    for center in colours:
-        for ind, c in zip(range(9), colours[center]):
-            poly3Dobjects[f'{center}{ind}'].set_facecolor(col_con[c])
-    fig.canvas.draw_idle()
+        elif rot[0] + rot[1] in 'RBOGR':
+            if rot[2] == 0:
+                # vert[2] == 3 inverted rotation Z
+                parsed.extend([['Z', 2, 3, -STEPS]] * STEPS)
 
-def rotate1():
-    global COLOURS
-    global INDEX 
+            elif rot[2] == 2: 
+                # vert[2] == - 3 inverted rotation Z
+                parsed.extend([['Z', 2, -3, -STEPS]] * STEPS)
 
-    a.rotate('R', *COLOURS[INDEX % 4])
-    INDEX = (INDEX + 1) % 4
-    update_3d_plot()
+        elif rot[0] + rot[1] in 'RWOYR':
+            if rot[2] == 0:
+                # vert[1] == -3 original rotation Y
+                parsed.extend([['Y', 1, -3, STEPS]] * STEPS)
+                
+            elif rot[2] == 2: 
+                # vert[1] == 3 original rotation Y
+                parsed.extend([['Y', 1, 3, STEPS]] * STEPS)
 
-def Solve():
-    try:
-        b.rotate(*SOLUTION_ORDER.pop())
-        update_3d_plot()
+        elif rot[0] + rot[1] in 'BWGYB':
+            if rot[2] == 0:
+                # vert[0] == 3 original rotation X
+                parsed.extend([['X', 0, 3, STEPS]] * STEPS)
+            elif rot[2] == 2: 
+                # vert[0] == -3 original rotation X
+                parsed.extend([['X', 0, -3, STEPS]] * STEPS)
+        elif rot[0] + rot[1] in 'RGOBR':
+            if rot[2] == 0:
+                # vert[2] == 3 original rotation Z
+                parsed.extend([['Z', 2, -3, STEPS]] * STEPS)
+            elif rot[2] == 2: 
+                # vert[2] == -3 original rotation Z
+                parsed.extend([['Z', 2, 3, STEPS]] * STEPS)
+    return parsed[::-1]
 
-    except IndexError:
-        pass
-
-
-# def top(event):
-
-# def top_prime(event):
-
-# def left(event):
-
-# def left_prime(event):
-
-# def right(event):
-
-# def right_prime(event):
-
-# def bottom(event):
-
-# def bottom_prime(event):
-
-if __name__ == '__main__':
-    import time
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    plt.axis('off')
-    ax.grid(False)
-    ax.set_aspect('equal')
-    INDEX = 0
-    COLOURS = [('Y', 2), ('B', 2), ('W', 0), ('G', 0)]
-    # plt.show()
-    # ax.set_xticks([])
-    # ax.set_yticks([])
-    # ax.set_zticks([])
-    # ax.set_xlabel('x axis')
-    # ax.set_ylabel('y axis')
-    # ax.set_zlabel('z axis')
-    lowest = float('inf')
-    a = Solution(Rubiks_Cube().faces)
-    scramble = a.scramble()
-    # print(scramble)
-    for COLOUR in 'RGWYBO':
-        total = 0
-
-        op, ROTATION_ORDER = a.Cross('Y')
-        total += op
-
-        op, rotation = a.Corners('Y')
-        ROTATION_ORDER.extend(rotation)
-        total += op
-
-        op, rotation = a.Fix_sides('Y')
-        ROTATION_ORDER.extend(rotation)
-        total += op
-
-        op, rotation = a.Top_cross('Y')
-        ROTATION_ORDER.extend(rotation)
-        total += op
-
-        op, rotation = a.Top_corners('Y')
-        ROTATION_ORDER.extend(rotation)
-        total += op
-
-        op, rotation = a.Invert('Y')
-        ROTATION_ORDER.extend(rotation)
-        total += op
-
-        op, rotation = a.Top_corners('Y')
-        ROTATION_ORDER.extend(rotation)
-        total += op
-
-        op, rotation = a.Final_Solve('Y')
-        ROTATION_ORDER.extend(rotation)
-        total += op
-
-        ROTATION_ORDER = ROTATION_ORDER[::-1]
-        COPY = ROTATION_ORDER[:]
-
-        for i in scramble:
-            a.rotate(*i)
-
-        if total < lowest:
-            lowest = total
-            SOLUTION_ORDER = ROTATION_ORDER
-        
-    # print(a)
-    # print(ROTATION_ORDER)
-    b = Solution(Rubiks_Cube().faces)
-
-    for i in scramble:
-        b.rotate(*i)
-
-    print(lowest)
-
-    # a.scramble()
-    # a.Cross('G')
-    # a.Corners('G')
-    poly3Dobjects = plot(b)
-
-    timer = fig.canvas.new_timer(interval=100)
-    timer.add_callback(Solve)
-    timer.start()
-
-    # button_ax = plt.axes([0.8, 0.025, 0.1, 0.04])
-    # color_button = Button(button_ax, "L'", hovercolor='0.975')
-    # color_button.on_clicked(left_prime)
+ax.set_aspect('equal')
+ax.set_xlim3d(-3, 3)
+ax.set_ylim3d(-3, 3)
+ax.set_zlim3d(-3, 3)
+plt.axis('off')
+# RIGHT_ALG = [['R', 'Y', 2], ['R', 'B', 2], ['R', 'W', 0], ['R', 'G', 0]] # test1
+# RIGHT_ALG = [['W', 'R', 2], ['W', 'B', 2], ['W', 'O', 0], ['W', 'G', 0]]
+CUBE = Solution()
+SCRAMBLE = CUBE.Scramble()
+ROTATION_ORDER = parse_rotation(SCRAMBLE, 1)
+# print(ROTATION_ORDER)
+while rotation():
+    pass
+STEPS, ROTATIONS = CUBE.Solve()
+print(STEPS)
+ROTATION_ORDER = parse_rotation(ROTATIONS)
+timer = fig.canvas.new_timer(interval=50)
+timer.add_callback(rotation)
+timer.start()
+plt.show()           
 
 
 
-    plt.show()
-    # ax.scatter(*rotate(X, Y, Z, times=1), c='black', marker= ' ')
-    # ax.scatter(*rotate(X, Y, Z, axis = 'Y', times=1), c='black', marker=' ')
 
-    # print(colours)
-    # generate(colours['G'], 'G', *rotate(X, Y, Z, times=1))
-    # generate(colours['O'], 'O', *rotate(X, Y, Z, times=2))
-    # generate(colours['B'], 'B', *rotate(X, Y, Z, times=3))
-    # generate(colours['Y'], 'Y', *rotate(X, Y, Z, axis = 'Y', times=-1))
-    # generate(colours['W'], 'W', *rotate(X, Y, Z, axis = 'Y', times=1))
-    # generate(colours['R'], 'R', X, Y, Z)
-    # plt.yscale('linear')
-    # plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
